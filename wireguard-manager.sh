45d1818b95330ec7da71ef9b71f52440839f443e
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-09-14 08:13:55 -0400

    [Update] Added support for amazon virtualization
    Signed-off-by: fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>

--- wireguard-manager.sh
+++ wireguard-manager.sh
@@ -69,7 +69,7 @@ installing-system-requirements
 function virt-check() {
   CURRENT_SYSTEM_VIRTUALIZATION=$(systemd-detect-virt)
   case ${CURRENT_SYSTEM_VIRTUALIZATION} in
-  "kvm" | "none" | "qemu" | "lxc" | "microsoft" | "vmware" | "xen") ;;
+  "kvm" | "none" | "qemu" | "lxc" | "microsoft" | "vmware" | "xen" | "amazon") ;;
   *)
     echo "${CURRENT_SYSTEM_VIRTUALIZATION} virtualization is not supported (yet)."
     exit
