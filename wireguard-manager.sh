820efed47db6d1fd1135c788c6c45ce2875761b4
fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>
2022-09-16 20:06:16 -0400

    Update the ip in the client files as well
    Signed-off-by: fX4N1DmH311X39SpxXLWxMWy49n6o8zb <102563715+fX4N1DmH311X39SpxXLWxMWy49n6o8zb@users.noreply.github.com>

--- wireguard-manager.sh
+++ wireguard-manager.sh
@@ -1478,6 +1478,16 @@ PublicKey = ${SERVER_PUBKEY}" >>${WIREGUARD_CLIENT_PATH}/"${NEW_CLIENT_NAME}"-${
       if [ "${OLD_SERVER_HOST}" != "${NEW_SERVER_HOST}" ]; then
         sed --in-place "1s/${OLD_SERVER_HOST}/${NEW_SERVER_HOST}/" ${WIREGUARD_CONFIG}
       fi
+      COMPLETE_CLIENT_LIST=$(grep start ${WIREGUARD_CONFIG} | cut --delimiter=" " --fields=2)
+      for CLIENT_LIST_ARRAY in ${COMPLETE_CLIENT_LIST}; do
+        USER_LIST[ADD_CONTENT]=${CLIENT_LIST_ARRAY}
+        ADD_CONTENT=$(("${ADD_CONTENT}" + 1))
+      done
+      for CLIENT_NAME in "${USER_LIST[@]}"; do
+        if [ -f "${WIREGUARD_CLIENT_PATH}/${CLIENT_NAME}-${WIREGUARD_PUB_NIC}.conf" ]; then
+          sed --in-place "1s/${OLD_SERVER_HOST}/${NEW_SERVER_HOST}/" ${WIREGUARD_CLIENT_PATH}/${CLIENT_NAME}-${WIREGUARD_PUB_NIC}.conf
+        fi
+      done
       ;;
     13) # Change the wireguard interface's port number.
       OLD_SERVER_PORT=$(head --lines=1 ${WIREGUARD_CONFIG} | cut --delimiter=" " --fields=4 | cut --delimiter=":" --fields=2)
